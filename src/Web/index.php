066be487a5770ecc4912907967478beedc3e4b5f
aldemirojunior <lelekojunior18@gmail.com>
2022-11-20 22:51:59 -0300

    first commit

---
+++ src/Web/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<title>Cadastro Presenca</title>
+<body>
+
+<?php
+echo "<script>alert('puts ate um Js');</script>";?>
+<?php echo "<h1>Essa linha foi processada antes de chegar no navegador, era um codigo php!</h1>";?>
+
+<h1>This is a heading</h1>
+<p>This is a paragraph.</p>
+
+</body>
+</html>
